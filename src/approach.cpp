901e528c1b399b66d1a52f5b3bb72bfd08135762
jacksongayda <jacksongayda@gmail.com>
2019-07-03 20:54:23 -0700

    Started on retrieve.cpp

---
+++ src/approach.cpp
@@ -0,0 +1,13 @@
+/* APPROACH MODE
+ * 
+ * STATUS : Robot has either detected a post on the left or the right side. 
+ * 
+ * PROTOCOL : Turn to position robot such that it is now following the tape leading towards the infinity stone post.
+ *            End loop when robot has reached the end of the tape and is now ready to begin retrieving to stone.
+ * 
+ * POSSIBLE PREVIOUS MODES : Search Mode
+ * 
+ * POSSIBLE NEXT MODES : Retrieval Mode
+ *                       Pathfinder Mode
+ *           
+ */
